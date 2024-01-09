8a9833a67fe2481acb286ac24911718b3f757203
Ryan Stewart <ryanstew17@gmail.com>
2024-01-09 12:15:13 -0400

    main function of game done

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const timeLeft = document.querySelector('#time-left');
 const score = document.querySelector('#score');
 
 let result = 0
+let hitPostition
+let currentTime = 60
 
 function randomSquare() {
   squares.forEach(square => {
@@ -12,11 +14,35 @@ function randomSquare() {
 
   let randomSquare = squares[Math.floor(Math.random() * 9)]
 randomSquare.classList.add('mole')
+
+hitPostition = randomSquare.id
 }
+
+squares.forEach(square => {
+  square.addEventListener('mousedown', () => {
+    if (square.id == hitPostition) {
+      result++
+      score.textContent = result
+      hitPostition = null
+    }
+  })
+})
 
 function moveMole() {
   let timerId = null 
-  timerId = setInterval(randomSquare, 500)
+  timerId = setInterval(randomSquare, 800)
 }
 
 moveMole()
+
+function countDown() {
+currentTime--
+timeLeft.textContent = currentTime
+
+if (currentTime == 0) {
+  clearInterval(countDownTimerId)
+  alert('GAMEOVER! Your final score is ' + result)
+}
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
